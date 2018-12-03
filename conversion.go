15f0dcf12b5b3e7f4c78c531d9e0945f576087dd
Ian <yiyuanl@andrew.cmu.edu>
2018-12-03 01:38:36 -0500

    update conversion.go according to objects changes

--- conversion.go
+++ conversion.go
@@ -1,6 +1,9 @@
 package main
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+)
 
 //TreeTraversal visit every node in the tree and return the *Vesicle
 //Create a vesicle for each iteration
@@ -8,8 +11,8 @@ import "strconv"
 //           this vesicle to the vesicle.
 //Recurssive case: call the recurssion for the child, add the vesicle to the vesicle
 func TreeTraversal(parseTree ParseTree) *Vesicle {
-	var vesicle *Vesicle
-	vesicle.vesicles = make([]*Vesicle, 0)
+	vesicle := new(Vesicle) //the new function return a pointer to the object
+	(*vesicle).vesicles = make([]*Vesicle, 0)
 
 	if parseTree != nil {
 		if parseTree.children != nil {
@@ -17,33 +20,43 @@ func TreeTraversal(parseTree ParseTree) *Vesicle {
 				//if it's == nil means now we are on the second last level
 				if child.children == nil {
 					//append the vesicle we created to the vesicle in this level
+					//fmt.Println("reach the leaves the current node is: ", (*parseTree).name, child.name)
 					vesicle.vesicles = append(vesicle.vesicles, GenerateProtein(parseTree))
 				} else {
 					//if it it not the second last level, then call TreeTraversal
-
+					//fmt.Println("not the leaves the current node is: ", (*parseTree).name, child.name)
 					childvesicle := TreeTraversal(child)
 
 					//Assign the order of vesicle operation on receptor's LocSignalRec
 					//in every level
-					(*childvesicle).receptorList[num].locSignalRec = num
-					vesicle.vesicles = append(vesicle.vesicles, childvesicle)
-
-					if parseTree.name == "IF" {
-						var ifKinase *IfKinase
-						//the receptorName will be the "code"
-						ifKinase.receptorName = parseTree.children[1].name
-						//the recognizeVesicleName will be the first one(condition)
-						ifKinase.recognizeVesicleName = parseTree.children[0].name
-						//vesicle.proteinList = append(vesicle.proteinList, ifKinase)
-					} else if parseTree.name == "WHILE" {
-						var ifKinase *IfKinase
-						//the receptorName will be the "code"
-						ifKinase.receptorName = parseTree.children[1].name
-						//the recognizeVesicleName will be the first one(condition)
-						ifKinase.recognizeVesicleName = parseTree.children[0].name
-						//vesicle.proteinList = append(vesicle.proteinList, ifKinase)
-						//// TODO: how to add value to subtrate? label it go back and forth
+					//fmt.Println("current node is ", parseTree.name, ", child is ", child.name)
+					//fmt.Println("the receptor name is ", (*childvesicle).receptorList[0])
+					/******************
+					Solve this problem, the locsignalRec doesn't work
+					******************/
+					for i := 0; i < len((*childvesicle).receptorList); i++ {
+						(*childvesicle).receptorList[i].locSignalRec = num
+						fmt.Println((*childvesicle).receptorList[i].locSignalRec)
 					}
+
+					vesicle.vesicles = append(vesicle.vesicles, childvesicle)
+					if parseTree.name == "IF" {
+						ifKinase := new(IfKinase)
+						//the receptorName will be the "code"
+						ifKinase.receptorName = parseTree.children[1].name
+						//the recognizeVesicleName will be the first one(condition)
+						ifKinase.recognizeVesicleName = parseTree.children[0].name
+						vesicle.ifKinase = ifKinase
+					} else if parseTree.name == "WHILE" {
+						ifKinase := new(IfKinase)
+						//the receptorName will be the "code"
+						ifKinase.receptorName = parseTree.children[1].name
+						//the recognizeVesicleName will be the first one(condition)
+						ifKinase.recognizeVesicleName = parseTree.children[0].name
+						vesicle.ifKinase = ifKinase
+						//// TODO: how to add label for the while loop???????
+					}
+
 				}
 			}
 
@@ -54,26 +67,27 @@ func TreeTraversal(parseTree ParseTree) *Vesicle {
 
 //GenerateProtein create the basic vesicle in the last two level
 func GenerateProtein(parseTree ParseTree) *Vesicle {
-	var receptor *Receptor
-	var vesicle *Vesicle
+	receptor := new(Receptor)
+	vesicle := new(Vesicle)
 	vesicle.InitializeVesicle()
 
 	for i := 0; i < len((*parseTree).children); i++ {
 
 		if parseTree.name == "COND" {
-			var checkerKinase *CheckerKinase
-			var receptor2 *Receptor
+			checkerKinase := new(CheckerKinase)
+			receptor2 := new(Receptor)
 
 			(*receptor).name = (*parseTree.children[0]).name
 			(*checkerKinase).name = (*parseTree.children[1]).name
 			(*receptor2).name = (*parseTree.children[2]).name
 
 			//vesicle.proteinList = append(vesicle.proteinList, checkerKinase)
-			//vesicle.receptorList = append(vesicle.receptorList, receptor)
-			//vesicle.receptorList = append(vesicle.recpeotrList, receptor)
+			vesicle.receptorList = make([]*Receptor, 0)
+			vesicle.receptorList = append(vesicle.receptorList, receptor)
+			vesicle.receptorList = append(vesicle.receptorList, receptor2)
 
 		} else if parseTree.name == "ARTH" {
-			var glucotrans *Glucotrans
+			glucotrans := new(Glucotrans)
 
 			(*receptor).name = (*parseTree.children[0]).name
 			number, _ := strconv.Atoi((*parseTree.children[2]).name)
@@ -82,17 +96,17 @@ func GenerateProtein(parseTree ParseTree) *Vesicle {
 			} else if (*parseTree.children[1]).name == "-" {
 				(*glucotrans).glucoCount = -number
 			}
-			//vesicle.proteinList = append(vesicle.proteinList, glucotrans)
-			//vesicle.receptorList = append(vesicle.receptorList, receptor)
+			vesicle.receptorList = make([]*Receptor, 0)
+			vesicle.glucoTrans = glucotrans
+			vesicle.receptorList = append(vesicle.receptorList, receptor)
 
 		} else if parseTree.name == "ASSIGN" {
-			var glucotrans *Glucotrans
-
-			(*receptor).name = (*parseTree.children[0]).name
-			number, _ := strconv.Atoi((*parseTree.children[1]).name)
+			glucotrans := new(Glucotrans)
+			(*receptor).name = (*parseTree.children[1]).name
+			number, _ := strconv.Atoi((*parseTree.children[0]).name)
 			(*glucotrans).glucoCount = number
-			//vesicle.proteinList = append(vesicle.proteinList, glucotrans)
-			//vesicle.receptorList = append(vesicle.receptorList, receptor)
+			vesicle.glucoTrans = glucotrans
+			vesicle.receptorList = append(vesicle.receptorList, receptor)
 		}
 	}
 	return vesicle
